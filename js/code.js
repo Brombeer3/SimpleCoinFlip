78de21bc227c16cada4e1d181184b9eee96ae0ee
Brombeer3 <108696044+Brombeer3@users.noreply.github.com>
2022-08-03 10:28:04 +0200

    Added first functionality.
    -Coin Flip
    -Links to Contact

---
+++ js/code.js
@@ -0,0 +1,18 @@
+// Funktion für zufälligen Münzwurf
+
+function coinFlip() {
+var coin = Math.random();
+
+    coin = (coin * 100) + 1;
+
+    coin = Math.floor(coin);
+
+  //  console.log(coin);
+
+    if (coin <= 50) {
+    alert("Heads!/Kopf!");
+  }
+  else {
+    alert("Tails!/Zahl!");
+  }
+}
